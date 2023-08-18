2dfc529face855a42a9a6092c962e7b40162f5ef
cqq <945327638@qq.com>
2023-08-18 16:18:34 +0800

    feat: 增加首页接单静态逻辑

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -16,28 +16,67 @@
     </map>
     <view class="location-panel">
       <tm-sheet :round="3" :shadow="2">
-        <view class="from" @tap="setFromOrToLocation('from')">
-          <tm-input disabled v-model="takeCarInfo.from.address" :followTheme="false" placeholder="请选择出发地" :margin="[0, 24]"></tm-input>
+        <view v-if="isTakingOrders" class="flex flex-col flex-center">
+          <view class="text-weight-b text-size-g">正在接单中...</view>
+          <view class="my-10 text-size-g">
+            {{
+              `${timeIncrease.timeDateTypeInfo.value.hours}:${timeIncrease.timeDateTypeInfo.value.minutes}:${timeIncrease.timeDateTypeInfo.value.seconds}`
+            }}
+          </view>
         </view>
-        <view class="to" @tap="setFromOrToLocation('to')">
-          <tm-input disabled v-model="takeCarInfo.to.address" :followTheme="false" placeholder="请选择目的地" :margin="[0, 24]"></tm-input>
-        </view>
+        <loading-button
+          v-if="isTakingOrders"
+          :block="true"
+          :click-fun="cancelTakingOrdersHandle"
+          :margin="[10]"
+          :shadow="0"
+          size="large"
+          label="取消接单"
+        ></loading-button>
+        <loading-button
+          v-if="!isTakingOrders"
+          :block="true"
+          :click-fun="startTakingOrdersHandle"
+          :margin="[10]"
+          :shadow="0"
+          size="large"
+          label="开始接单"
+        ></loading-button>
       </tm-sheet>
     </view>
-    <tabbar-nav>111</tabbar-nav>
+    <tm-drawer :width="300" :height="500" :hideHeader="true" :overlayClick="false" ref="popRef" placement="bottom">
+      <view class="pop-content">
+        <view class="text-weight-b text-size-g">距离客人约4.6KM</view>
+        <view class="my-10 text-size-g">
+          {{
+            `${timeCountdown.timeDateTypeInfo.value.hours}:${timeCountdown.timeDateTypeInfo.value.minutes}:${timeCountdown.timeDateTypeInfo.value.seconds}`
+          }}
+        </view>
+        <view class="flex flex-row">
+          <loading-button
+            :width="200"
+            :click-fun="cancelTakingOrdersForCustomerHandle"
+            :margin="[30]"
+            :shadow="0"
+            size="large"
+            label="取消"
+            type="info"
+          ></loading-button>
+          <loading-button :width="200" :click-fun="confirmTakingOrdersHandle" :margin="[30]" :shadow="0" size="large" label="接单"></loading-button>
+        </view>
+      </view>
+    </tm-drawer>
+    <tabbar-nav></tabbar-nav>
   </tm-app>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useQqMapSdk } from '@/hooks/useQqMapSdk'
 import { IMapProps } from '@/api/index/types'
 import { useTakeCarInfoStore } from '@/store/modules/takeCarInfo'
-import { QqMapkey } from '@/config/config'
-const { qqmapsdk } = useQqMapSdk()
+import { useTimeIncrease } from '@/hooks/useTimeIncrease'
+import tmDrawer from '@/tmui/components/tm-drawer/tm-drawer.vue'
+import { useCountdown } from '@/hooks/useCountdown'
 //#region <map相关>
 const map = uni.createMapContext('map')
-// 选择地点插件
-const chooseLocation = requirePlugin('chooseLocation')
 // 打车相关信息仓库
 const takeCarInfo = useTakeCarInfoStore()
 const mapProps = ref<Pick<IMapProps, 'longitude' | 'latitude'>>({
@@ -46,51 +85,76 @@ const mapProps = ref<Pick<IMapProps, 'longitude' | 'latitude'>>({
   // 中心纬度
   latitude: 39.908675
 })
-// 选择地点的标识
-const flag = ref<'from' | 'to'>('from')
 
 //  回到初始位置
 function moveToLocationHandle() {
   map.moveToLocation(mapProps.value)
 }
-// 设置出发地、目的地位置
-function setFromOrToLocation(type: 'from' | 'to') {
-  // 设置标识
-  flag.value = type
-  const key = QqMapkey
-  const referer = import.meta.env.VITE_APP_TITLE //调用插件的app的名称
-  const location = JSON.stringify({
-    latitude: mapProps.value.latitude,
-    longitude: mapProps.value.longitude
-  })
-  uni.navigateTo({
-    url: `plugin://chooseLocation/index?key=${key}&referer=${referer}&location=${location}`
-  })
-  console.log(key, referer)
-}
 
 // 样式
 //#endregion
+
+//#region <弹出层>
+// 打开弹出层
+const popRef = ref<InstanceType<typeof tmDrawer>>()
+// 接单倒计时
+const timeCountdown = useCountdown({
+  // 倒计时长
+  seconds: 10,
+  // 回调函数,到达持续时长后执行
+  callback: () => {
+    console.log('倒计时结束')
+    closePopupHandle()
+  }
+})
+function openPopupHandle() {
+  popRef.value?.open()
+  console.log('打开弹出层openPopupHandle')
+}
+// 关闭弹出层
+function closePopupHandle() {
+  popRef.value?.close()
+  console.log('关闭弹出层closePopupHandle')
+}
+// 确认接单
+function confirmTakingOrdersHandle() {
+  console.log('确认接单confirmTakingOrdersHandle')
+}
+// 取消接单
+function cancelTakingOrdersForCustomerHandle() {
+  closePopupHandle()
+  console.log('取消接单cancelTakingOrdersForCustomerHandle')
+}
+// 接收到推送订单
+function receivePushOrderHandle() {
+  openPopupHandle()
+  timeCountdown.start()
+}
+//#endregion
+
+//#region <等待订单>
+// 时间增长
+const timeIncrease = useTimeIncrease()
+// 接单标识,是否接单
+const isTakingOrders = ref(false)
+// 开始接单
+function startTakingOrdersHandle() {
+  console.log('开始接单startTakingOrdersHandle')
+  isTakingOrders.value = true
+  timeIncrease.start()
+}
+// 取消接单
+function cancelTakingOrdersHandle() {
+  console.log('取消订单cancelOrderHandle')
+  isTakingOrders.value = false
+  timeIncrease.stopAndReset()
+}
+//#endregion
+
 onShow(() => {
-  const location = chooseLocation.getLocation() // 如果点击确认选点按钮，则返回选点结果对象，否则返回null
-  console.log('location', location)
-  // 如果没有选择地点，重置takeCarInfo中所有信息
-  if (!location) {
-    takeCarInfo.$reset()
-    return
-  }
-  // 如果有选择地点，则设置出发地或目的地
-  if (flag.value === 'from') {
-    takeCarInfo.from = location
-  } else {
-    takeCarInfo.to = location
-  }
-  // 如果出发地和目的地都有值，则跳转到下单页面
-  if (takeCarInfo.from.address && takeCarInfo.to.address) {
-    uni.navigateTo({
-      url: '/pages/creatOrder/creatOrder'
-    })
-  }
+  setTimeout(() => {
+    receivePushOrderHandle()
+  }, 1000)
 })
 onLoad(() => {
   //   获取当前位置信息
@@ -101,12 +165,6 @@ onLoad(() => {
       mapProps.value.latitude = res.latitude
     }
   })
-})
-onHide(() => {
-  chooseLocation.setLocation(null)
-})
-onUnload(() => {
-  chooseLocation.setLocation(null)
 })
 </script>
 
@@ -127,5 +185,13 @@ onUnload(() => {
   //background: pink;
   width: 100%;
   bottom: 100rpx;
+}
+.pop-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30rpx 20rpx 10rpx;
+  height: 400rpx;
 }
 </style>
