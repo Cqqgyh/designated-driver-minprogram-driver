f664b83175edbd0932612311f3cfa66a4054cbbf
cqq <945327638@qq.com>
2023-08-22 16:22:53 +0800

    feat: 封装upload-images组件

---
+++ src/components/upload-images/upload-images.vue
@@ -0,0 +1,8 @@
+<template>
+  <tm-upload v-bind="$attrs" :url="upLoadUrl"></tm-upload>
+</template>
+<script setup lang="ts">
+const upLoadUrl = ref(import.meta.env.VITE_APP_UPLOAD_URL as string)
+</script>
+
+<style scoped></style>
