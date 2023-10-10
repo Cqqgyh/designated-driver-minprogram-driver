9e78438c731b2d5837ad236ae76437089ad94b32
cqq <945327638@qq.com>
2023-10-10 15:49:23 +0800

    perf: 优化图片上传组件内部逻辑

--- src/components/upload-images/upload-images.vue
+++ src/components/upload-images/upload-images.vue
@@ -1,8 +1,31 @@
 <template>
-  <tm-upload v-bind="$attrs" :url="upLoadUrl"></tm-upload>
+  <tm-upload v-bind="$attrs" :url="upLoadUrl" @success="success" @remove="remove"></tm-upload>
 </template>
 <script setup lang="ts">
+import { useAttrs } from 'vue'
 const upLoadUrl = ref(import.meta.env.VITE_APP_UPLOAD_URL as string)
+const attrs = useAttrs()
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps(['modelValue'])
+const success = (item: any, fileList: any) => {
+  console.log('success', item, fileList)
+  let targetList = fileList.map((item: any) => {
+    const targetItem = JSON.parse(item.response)
+    return {
+      url: targetItem.data,
+      name: targetItem.data
+    }
+  })
+  emit('update:modelValue', targetList)
+}
+const remove = (item: any) => {
+  const targetItem = item.response ? JSON.parse(item.response) : { data: item.url }
+  let targetList = props.modelValue.filter((item2: any) => {
+    return item2.url !== targetItem.data
+  })
+  emit('update:modelValue', targetList)
+}
 </script>
 
 <style scoped></style>
