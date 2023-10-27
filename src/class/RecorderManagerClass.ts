cffc0f9b94e8e905faf42343c46ab2e081ac96ae
cqq <945327638@qq.com>
2023-10-27 15:40:44 +0800

    feat: 构建录音class

--- src/class/RecorderManagerClass.ts
+++ src/class/RecorderManagerClass.ts
@@ -1,8 +1,9 @@
 /**
  * @description 微信录音管理器类
  */
+const plugin = requirePlugin('WechatSI')
 interface IRecorderManagerClass {
-  //  录音最大时长 单位秒
+  //  录音最大时长 单位s
   maxDuration?: number
   //  录音达到最大时长回调函数
   recordCallback?: (params: IRecordCallback) => void
@@ -11,24 +12,16 @@ export interface IRecordCallback {
   tempFilePath: string
   duration: number
   fileSize: number
-  startTime: number
-  endTime: number
-  recordTime: number
+  result: string
 }
 
 export class RecorderManagerClass {
   //   录音管理器实例
   private recorderManager: any
-  //   录音文件的临时路径
-  private tempFilePath: string
   //   录音文件的持续时长
   private readonly maxDuration: number
   //   是否正在录制
   private isRecording: boolean
-  //   是否暂停
-  private isPaused: boolean
-  //   是否停止
-  private isStoped: boolean
   //   录音开始时间
   private startTime: number
   //   录音结束时间
@@ -40,12 +33,8 @@ export class RecorderManagerClass {
   //   录音时长回调函数
   private readonly recordCallback: (params: IRecordCallback) => void
   constructor(params?: IRecorderManagerClass) {
-    this.recorderManager = wx.getRecorderManager()
-    this.tempFilePath = ''
     this.maxDuration = params?.maxDuration || 5
     this.isRecording = false
-    this.isPaused = false
-    this.isStoped = false
     this.startTime = 0
     this.endTime = 0
     this.recordTime = 0
@@ -57,48 +46,18 @@ export class RecorderManagerClass {
     if (this.isRecording) {
       return
     }
+    this.recorderManager = plugin.getRecordRecognitionManager()
     this.isRecording = true
-    this.isPaused = false
-    this.isStoped = false
     this.startTime = new Date().getTime()
-    this.recorderManager.start({
-      format: 'mp3'
-    })
     // 监听录音结束事件
     this.onRecordEnd()
-    // 监听录音因为受到系统占用而被中断开始事件
-    this.onRecordInterruptionBegin()
-    // 监听录音中断结束事件
-    this.onRecordInterruptionEnd()
+    //  开始录音
+    this.recorderManager.start({ duration: 60000, lang: 'zh_CN' })
+
     //  开始录音后，每隔1s计算一次录音时长
     this.recordTimer = setInterval(() => {
       //  如果录音时长达到最大时长，则停止录音
       console.log('this.recordTime', this.recordTime)
-
-      if (this.maxDuration && this.recordTime >= this.maxDuration) {
-        this.stopRecord()
-      }
-      this.recordTime++
-    }, 1000)
-  }
-  //   暂停录音
-  public pauseRecord() {
-    if (!this.isRecording || this.isPaused) {
-      return
-    }
-    this.isPaused = true
-    this.recorderManager.pause()
-    clearInterval(this.recordTimer)
-  }
-  //   继续录音
-  public resumeRecord() {
-    if (!this.isRecording || !this.isPaused) {
-      return
-    }
-    this.isPaused = false
-    this.recorderManager.resume()
-    this.recordTimer = setInterval(() => {
-      //  如果录音时长达到最大时长，则停止录音
       if (this.maxDuration && this.recordTime >= this.maxDuration) {
         this.stopRecord()
       }
@@ -107,52 +66,33 @@ export class RecorderManagerClass {
   }
   //   停止录音
   public stopRecord() {
-    if (!this.isRecording || this.isStoped) {
+    console.log('停止录音-1')
+    if (!this.isRecording) {
       return
     }
+    console.log('停止录音-2')
     this.isRecording = false
-    this.isStoped = true
     this.endTime = new Date().getTime()
     this.recorderManager.stop()
+    this.recorderManager = null
     clearInterval(this.recordTimer)
-  }
-  // 监听录音因为受到系统占用而被中断开始事件
-  public onRecordInterruptionBegin() {
-    this.recorderManager.onInterruptionBegin(() => {
-      this.pauseRecord()
-    })
-  }
-  // 监听录音中断结束事件
-  public onRecordInterruptionEnd() {
-    this.recorderManager.onInterruptionEnd(() => {
-      this.resumeRecord()
-    })
   }
   //   监听录音结束事件
   public onRecordEnd() {
-    // tempFilePath 	string 	录音文件的临时路径 (本地路径)
-    // duration 	number 	录音总时长，单位：ms
-    // fileSize 	number 	录音文件大小，单位：Byte
-    this.recorderManager.onStop((res: { tempFilePath: string; duration: number; fileSize: number }) => {
-      this.tempFilePath = res.tempFilePath
+    this.recorderManager.onStop = (res: { tempFilePath: string; duration: number; fileSize: number; result: string }) => {
       this.recordCallback({
-        tempFilePath: this.tempFilePath,
+        tempFilePath: res.tempFilePath,
         duration: res.duration,
         fileSize: res.fileSize,
-        startTime: this.startTime,
-        endTime: this.endTime,
-        recordTime: this.recordTime
+        result: res.result
       })
       //  重置所有参数
       this.reset()
-    })
+    }
   }
   //   重置所有参数
   public reset() {
-    this.tempFilePath = ''
     this.isRecording = false
-    this.isPaused = false
-    this.isStoped = false
     this.startTime = 0
     this.endTime = 0
     this.recordTime = 0
